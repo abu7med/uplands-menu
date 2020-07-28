35644672b56f7a243e7669620be86e5e181063a7
womba-womba <armin.zakeri@gmail.com>
2020-07-28 21:35:12 +0200

    small changes

---
+++ build/precache-manifest.28dc3663d29e17caab18a9519ba6e64c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2825d443c152d644e594a0bd4296060d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84a6e6a6e6ff1e541ec6",
+    "url": "/static/css/main.e3b0444c.chunk.css"
+  },
+  {
+    "revision": "56c95497cc8ce2bf612a",
+    "url": "/static/js/2.2a9be288.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.2a9be288.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a6e6a6e6ff1e541ec6",
+    "url": "/static/js/main.5e5745ca.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
